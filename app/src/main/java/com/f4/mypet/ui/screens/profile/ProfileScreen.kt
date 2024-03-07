7a020769083958a249c32ee4b5c2b69c345955b6
MamykinVsevolod <vsevolodmamykin@gmail.com>
2024-03-07 23:01:54 +0300

    update import

--- app/src/main/java/com/f4/mypet/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/f4/mypet/ui/screens/profile/ProfileScreen.kt
@@ -1,8 +1,5 @@
 package com.f4.mypet.ui.screens.profile
 
-import android.content.Context
-import android.content.Intent
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -28,11 +25,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.f4.mypet.R
-import kotlinx.coroutines.InternalCoroutinesApi
 import com.f4.mypet.ui.MyPetTopBar
 
 @Composable
