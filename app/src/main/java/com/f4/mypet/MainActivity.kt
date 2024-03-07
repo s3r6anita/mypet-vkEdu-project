5e6fb7b2c8880ad85a9715e788383ec5fde5ae85
MamykinVsevolod <vsevolodmamykin@gmail.com>
2024-03-07 23:04:04 +0300

    update mainactivity

--- app/src/main/java/com/f4/mypet/MainActivity.kt
+++ app/src/main/java/com/f4/mypet/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.f4.mypet.ui.screens.profile.CreateUpdateProfileScreen
-import com.f4.mypet.ui.screens.profile.ProfileScreen
+//import com.f4.mypet.ui.screens.profile.ProfileScreen
 import com.f4.mypet.ui.screens.profile.ListProfileScreen
 import com.f4.mypet.ui.theme.MyPetTheme
 
