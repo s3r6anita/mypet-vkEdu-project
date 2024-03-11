0b56b2aef5f65f1aabff4f6698ab8c3bed1c5267
ponomo2 <ponom.sofia03@gmail.com>
2024-03-11 16:58:22 +0300

    add daos

---
+++ app/src/main/java/com/f4/mypet/db/daos/MedRecordDAO.kt
@@ -0,0 +1,17 @@
+package com.f4.mypet.db.daos
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.f4.mypet.db.entities.MedRecord
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MedRecordDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(medRecord: MedRecord)
+
+    @Query("SELECT * from MedRecord where pet = :petId")
+    fun getMedRecords(petId: Int): Flow<List<MedRecord>>
+}
---
+++ app/src/main/java/com/f4/mypet/db/daos/PetDAO.kt
@@ -0,0 +1,20 @@
+package com.f4.mypet.db.daos
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.f4.mypet.db.entities.Pet
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface PetDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(pet: Pet)
+
+    @Query("SELECT * from pet")
+    fun getPets(): Flow<List<Pet>>
+
+
+}
