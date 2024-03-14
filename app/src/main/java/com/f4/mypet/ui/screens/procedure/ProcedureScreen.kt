2d9ce3480ef8a7c116cd7c2717ec897f1ecce446
MamykinVsevolod <vsevolodmamykin@gmail.com>
2024-03-14 19:10:59 +0300

    optimize imports

--- app/src/main/java/com/f4/mypet/ui/screens/procedure/ProcedureScreen.kt
+++ app/src/main/java/com/f4/mypet/ui/screens/procedure/ProcedureScreen.kt
@@ -35,16 +35,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
 import com.f4.mypet.ui.MyPetTopBar
 import com.f4.mypet.R
-import com.f4.mypet.navigation.Routes
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.util.Date
+
 
 @Composable
 fun ProcedureScreen() {
