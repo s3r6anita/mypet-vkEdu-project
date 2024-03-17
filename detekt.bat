a9d458cbaffde691b1fa0e4686c254e4b7c683e7
s3r6anita <xz200359@gmail.com>
2024-03-17 18:59:16 +0300

    fix local launch of detekt

--- detekt.bat
+++ detekt.bat
@@ -2,13 +2,15 @@
 TITLE Check style with Detekt
 SET ROOT_FOLDER=build\bin
 SET VERSION=1.23.5
-SET DETEKT_BIN=%ROOT_FOLDER%\detekt-%VERSION%.jar
+SET DETEKT_ZIP=detekt-cli-%VERSION%.zip
+SET DETEKT_BIN=%ROOT_FOLDER%\detekt-cli-%VERSION%-all.jar
 if not exist %DETEKT_BIN% (
   ECHO Please wait, first download...
   if not exist %ROOT_FOLDER% MKDIR %ROOT_FOLDER%
-  curl.exe -sL "https://github.com/detekt/detekt/releases/download/v%VERSION%/detekt-cli-%VERSION%-all.jar" -o "%DETEKT_BIN%"
+  curl.exe -sSLO "https://github.com/detekt/detekt/releases/download/v%VERSION%/detekt-cli-%VERSION%.zip"
+  7z e "%DETEKT_ZIP%" -o"%ROOT_FOLDER%"
+  del "%DETEKT_ZIP%"
 )
 
-java -jar %DETEKT_BIN% --config .github/workflows/assets/detekt.yml %*
+java -jar %DETEKT_BIN% --config .github/workflows/assets/detekt.yml
 echo Done!
-PAUSE
