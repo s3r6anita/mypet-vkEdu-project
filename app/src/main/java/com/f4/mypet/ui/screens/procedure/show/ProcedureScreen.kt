b40b5862d0ef39427fe33c9b15ecdbff6bbec6f8
s3r6anita <xz200359@gmail.com>
2024-04-01 16:57:34 +0300

    add data from db presentation

--- app/src/main/java/com/f4/mypet/ui/screens/procedure/show/ProcedureScreen.kt
+++ app/src/main/java/com/f4/mypet/ui/screens/procedure/show/ProcedureScreen.kt
@@ -3,20 +3,8 @@ package com.f4.mypet.ui.screens.procedure.show
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import com.f4.mypet.ui.theme.GreenButton
-import com.f4.mypet.ui.theme.LightBlueBackground
-
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -25,32 +13,55 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.f4.mypet.PetDateTimeFormatter
 import com.f4.mypet.R
+import com.f4.mypet.data.db.entities.ProcedureTitle
+import com.f4.mypet.data.db.entities.ProcedureType
+import com.f4.mypet.navigation.Routes
 import com.f4.mypet.ui.components.MyPetTopBar
 import com.f4.mypet.ui.screens.profile.show.TextComponent
 import com.f4.mypet.ui.theme.BlueCheckbox
+import com.f4.mypet.ui.theme.GreenButton
+import com.f4.mypet.ui.theme.LightBlueBackground
 import com.f4.mypet.ui.theme.RedButton
+import kotlinx.coroutines.launch
 
 @Composable
 fun ProcedureScreen(
@@ -58,12 +69,26 @@ fun ProcedureScreen(
     profileId: Int,
     procedureId: Int
 ) {
+    val scope = rememberCoroutineScope()
+    val viewModel: ProcedureViewModel = hiltViewModel()
+
+    LaunchedEffect(Unit) {
+        scope.launch {
+            viewModel.getProcedure(procedureId)
+        }
+    }
+    val procedure by viewModel.procedureUiState.collectAsState()
+    val title = viewModel.titles.find { title -> title.id == procedure.title }
+        ?: ProcedureTitle("Unknown", -1, -1)
+    val type =
+        viewModel.types.find { type -> type.id == title.type }
+            ?: ProcedureType("Unknown", -1)
+
     var openAlertDialog by remember { mutableStateOf(false) }
-    val dialogShape = RoundedCornerShape(12.dp)
-    val tmpIsDone = true
+
     if (openAlertDialog) {
         AlertDialog(
-            shape = dialogShape,
+            shape = RoundedCornerShape(12.dp),
             title = {
                 Text(text = stringResource(R.string.procedure_screen_delete_title_alert))
             },
@@ -96,15 +121,17 @@ fun ProcedureScreen(
                 }
             },
             containerColor = Color.White,
-            modifier = Modifier.shadow(elevation = 8.dp,shape = dialogShape)
+            modifier = Modifier.shadow(
+                elevation = 8.dp,
+                shape = RoundedCornerShape(12.dp)
+            )
         )
     }
 
     Scaffold(
         topBar = {
             MyPetTopBar(
-                //TODO: поменять на text = stringResource(R.string.procedure_screen_title),
-                text = "Процедура #$procedureId",
+                text = stringResource(R.string.procedure_screen_title),
                 canNavigateBack = true,
                 navigateUp = { navController.navigateUp() },
             )
@@ -119,13 +146,14 @@ fun ProcedureScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceBetween,
         ) {
-            Box(modifier = Modifier
-                .padding(vertical = 50.dp)
+            Box(
+                modifier = Modifier
+                    .padding(vertical = 50.dp)
             ) {
-                Card (
+                Card(
                     elevation = CardDefaults.cardElevation(
-                    defaultElevation = 6.dp
-                ),
+                        defaultElevation = 6.dp
+                    ),
                     colors = CardDefaults.cardColors(
                         containerColor = MaterialTheme.colorScheme.onSecondary,
                     ),
@@ -138,48 +166,56 @@ fun ProcedureScreen(
                             .padding(top = 50.dp),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Row (modifier = Modifier
-                            .fillMaxWidth(),
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
                             horizontalArrangement = Arrangement.Center,
                             verticalAlignment = Alignment.CenterVertically,
-                        ){
+                        ) {
                             Text(
-                                // TODO: потом заменить строку из ресурсов на данные из БД
-                                text = stringResource(R.string.procedure_screen_tmp_name),
+                                text = title.name,
                                 style = MaterialTheme.typography.headlineSmall,
                                 modifier = Modifier.weight(1f),
                             )
-                            if (tmpIsDone) {
+                            if (procedure.isDone == 1) {
                                 Image(
-                                    painter = painterResource(id = R.drawable.procedure_done_icon),
-                                    contentDescription = stringResource(id = R.string.procedure_screen_procedure_is_done),
+                                    imageVector = Icons.Filled.CheckCircle,
+                                    contentDescription = stringResource(id = R.string.procedure_screen_procedure_is_done)
                                 )
-                            }
-                            else {
+                            } else {
                                 Image(
-                                    painter = painterResource(id = R.drawable.procedure_not_done_icon),
-                                    contentDescription = stringResource(id = R.string.procedure_screen_procedure_is_not_done),
+                                    imageVector = Icons.Filled.Clear,
+                                    contentDescription = stringResource(id = R.string.procedure_screen_procedure_is_not_done)
                                 )
                             }
                         }
                         Spacer(modifier = Modifier.height(24.dp))
                         TextComponent(
-                            header = stringResource(R.string.procedure_screen_type), value = stringResource(R.string.procedure_screen_type)
+                            header = stringResource(R.string.procedure_screen_type),
+                            value = type.name
                         )
                         TextComponent(
-                            header = stringResource(R.string.procedure_screen_date_of_event), value = stringResource(R.string.procedure_screen_tmp_date)
+                            header = stringResource(R.string.procedure_screen_date_of_event),
+                            value = procedure.dateDone?.format(PetDateTimeFormatter.date)
+                                ?: "01.01.1001"
                         )
                         TextComponent(
-                            header = stringResource(R.string.procedure_screen_time_of_event), value = stringResource(R.string.procedure_screen_tmp_time)
+                            header = stringResource(R.string.procedure_screen_time_of_event),
+                            value = procedure.dateDone?.format(PetDateTimeFormatter.time)
+                                ?: "00:00"
                         )
                         TextComponent(
-                            header = stringResource(R.string.procedure_screen_frequency), value = stringResource(R.string.procedure_screen_tmp_frequency)
+                            header = stringResource(R.string.procedure_screen_frequency),
+                            value = procedure.frequency.toString()
                         )
                         TextComponent(
-                            header = stringResource(R.string.procedure_screen_reminder), value = stringResource(R.string.procedure_screen_tmp_time_for_proc)
+                            header = stringResource(R.string.procedure_screen_reminder),
+                            value = procedure.reminder?.format(PetDateTimeFormatter.dateTime)
+                                ?: "01.01.1001 00:00"
                         )
                         TextComponent(
-                            header = stringResource(R.string.procedure_screen_notice), value = stringResource(R.string.procedure_screen_tmp_notice)
+                            header = stringResource(R.string.procedure_screen_notice),
+                            value = procedure.notes
                         )
                     }
                 }
@@ -188,8 +224,9 @@ fun ProcedureScreen(
                     horizontalArrangement = Arrangement.Center
                 ) {
                     Image(
-                        painter = painterResource(id = R.drawable.some_procedure_icon),
-                        contentDescription = stringResource(id = R.string.procedure_screen_tmp_icon_procedure),
+                        // TODO: менять на иконку, соответствующую названию
+                        painter = painterResource(id = R.drawable.procedures_icon),
+                        contentDescription = stringResource(id = R.string.procedure_screen_icon_procedure_desc),
                         contentScale = ContentScale.Inside,
                         modifier = Modifier
                             .size(100.dp)
@@ -199,22 +236,26 @@ fun ProcedureScreen(
                 }
 
             }
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.Bottom,
-            ){
+            ) {
+                // кнопка редактирования
                 Button(
+                    contentPadding = PaddingValues(start = 1.dp, end = 1.dp),
+                    border = BorderStroke(1.dp, GreenButton),
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = GreenButton),
+                    onClick = {
+                        navController.navigate(Routes.UpdateProcedure.route + "/" + profileId + "/" + procedureId) {
+                            launchSingleTop = true
+                        }
+                    },
                     modifier = Modifier
                         .padding(bottom = 40.dp)
-                        .weight(1f),
-                    contentPadding = PaddingValues(start = 1.dp, end = 1.dp),
-                    onClick = {
-                        //TODO сделать навагацию
-                    },
-                    border = BorderStroke(1.dp, GreenButton),
-                    colors = ButtonDefaults.outlinedButtonColors(contentColor = GreenButton)
+                        .weight(1f)
                 ) {
                     Text(
                         text = stringResource(id = R.string.edit_button_description),
@@ -227,8 +268,7 @@ fun ProcedureScreen(
                 Button(
                     modifier = Modifier
                         .padding(bottom = 40.dp)
-                        .weight(1f)
-                    ,
+                        .weight(1f),
                     onClick = {
                         openAlertDialog = true
                     },
@@ -244,11 +284,5 @@ fun ProcedureScreen(
             }
 
         }
-
     }
-
-
-
 }
-
-//TODO removeProcedure
