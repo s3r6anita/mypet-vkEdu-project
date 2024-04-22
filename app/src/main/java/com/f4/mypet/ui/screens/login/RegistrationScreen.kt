d6eb1cee841e6e4d6d6049c29bd4d461b53aac0e
AlexandraShumailova <145394168+AlexandraShumailova@users.noreply.github.com>
2024-04-22 20:07:09 +0300

    feat(registration): RegistrationScreen

--- app/src/main/java/com/f4/mypet/ui/screens/login/RegistrationScreen.kt
+++ app/src/main/java/com/f4/mypet/ui/screens/login/RegistrationScreen.kt
@@ -18,21 +18,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.f4.mypet.R
 import com.f4.mypet.ui.components.MyPetTopBar
 import com.f4.mypet.ui.theme.GreenButton
-import com.f4.mypet.ui.theme.LightGrayTint
+import com.f4.mypet.ui.theme.Purple40
 
 @Composable
-fun HelloScreen(){
+fun RegistrationScreen(){
     Scaffold(
         topBar = {
             MyPetTopBar(
@@ -63,6 +63,38 @@ fun HelloScreen(){
                     style = MaterialTheme.typography.displayMedium
                 )
             }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 15.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.reg_title),
+                    color = Purple40,
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+
+            // name
+            OutlinedTextField(
+                value = "",
+                onValueChange = {  },
+                label = { Text(stringResource(id = R.string.reg_name_label)) },
+                placeholder = { Text(stringResource(id = R.string.reg_name_placeholder)) },
+                trailingIcon = {
+                    IconButton(onClick = {  }) {
+                        Icon(
+                            Icons.Default.Clear,
+                            contentDescription = stringResource(id = R.string.clear)
+                        )
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 15.dp),
+                singleLine = false,
+                shape = RoundedCornerShape(12.dp)
+            )
 
             // email
             OutlinedTextField(
@@ -90,7 +122,7 @@ fun HelloScreen(){
                 value = "",
                 onValueChange = {  },
                 label = { Text(stringResource(id = R.string.login_password_enter)) },
-                placeholder = { Text(stringResource(id = R.string.login_password_enter)) },
+                placeholder = { Text(stringResource(id = R.string.login_password_placeholder)) },
                 trailingIcon = {
                     IconButton(onClick = {  }) {
                         Icon(
@@ -99,32 +131,46 @@ fun HelloScreen(){
                         )
                     }
                 },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 15.dp),
+                singleLine = false,
+                shape = RoundedCornerShape(12.dp)
+            )
+
+            // password2
+            OutlinedTextField(
+                value = "",
+                onValueChange = {  },
+                label = { Text(stringResource(id = R.string.login_password_confirm)) },
+                placeholder = { Text(stringResource(id = R.string.login_password_placeholder)) },
+                trailingIcon = {
+                    IconButton(onClick = {  }) {
+                        Icon(
+                            Icons.Default.Clear,
+                            contentDescription = stringResource(id = R.string.clear)
+                        )
+                    }
+                },
+                visualTransformation = PasswordVisualTransformation(),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 20.dp),
                 singleLine = false,
                 shape = RoundedCornerShape(12.dp)
             )
+
+            //
             Button(
                 onClick = {  },
                 modifier = Modifier.fillMaxWidth(),
                 colors = ButtonDefaults.buttonColors(containerColor = GreenButton)
             ) {
                 Text(
-                    text = stringResource(id = R.string.login_button),
-                    textAlign = TextAlign.Center,
-                    color = Color.White
-                )
-            }
-
-            TextButton(
-                onClick = {  },
-                modifier = Modifier.fillMaxWidth(),
-            ) {
-                Text(
                     text = stringResource(id = R.string.login_registration_button),
                     textAlign = TextAlign.Center,
-                    color = LightGrayTint
+                    color = Color.White
                 )
             }
         }
