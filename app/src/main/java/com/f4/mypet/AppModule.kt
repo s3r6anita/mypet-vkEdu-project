4776918d38423e8d0ca0ef9d994c0836b58ff872
s3r6anita <xz200359@gmail.com>
2024-04-23 20:53:36 +0300

    minor changes

--- app/src/main/java/com/f4/mypet/AppModule.kt
+++ app/src/main/java/com/f4/mypet/AppModule.kt
@@ -4,14 +4,10 @@ import android.app.Application
 import com.f4.mypet.data.db.DBRepository
 import com.f4.mypet.data.db.PetDatabase
 import com.f4.mypet.data.db.Repository
-import com.f4.mypet.data.network.LoginService
-import com.f4.mypet.data.network.NetworkRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -35,21 +31,5 @@ object AppModule {
     @Provides
     fun provideDatabase(app: Application): PetDatabase {
         return PetDatabase.getDatabase(app)
-    }
-
-
-    private const val baseUrl = "http://mypet-backend-s3r6.amvera.io"
-
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl(baseUrl)
-        .build()
-
-    private val LoginRetrofitService: LoginService by lazy {
-        retrofit.create(LoginService::class.java)
-    }
-
-    val Repository: NetworkRepository by lazy {
-        NetworkRepository(LoginRetrofitService)
     }
 }
--- app/src/main/java/com/f4/mypet/data/network/NetworkRepository.kt
+++ app/src/main/java/com/f4/mypet/data/network/NetworkRepository.kt
@@ -1,7 +1,26 @@
 package com.f4.mypet.data.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class NetworkRepository(
     private val Service: LoginService
 ) {
-    suspend fun login(email: String, password: String) = LoginService.login()
+    suspend fun login(email: String, password: String) = LoginRetrofitService
+// TODO
+
+    private val baseUrl = "http://mypet-backend-s3r6.amvera.io/"
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(baseUrl)
+        .build()
+
+    private val LoginRetrofitService: LoginService by lazy {
+        retrofit.create(LoginService::class.java)
+    }
+
+    val Repository: NetworkRepository by lazy {
+        NetworkRepository(LoginRetrofitService)
+    }
 }
