48cb6f1994ce26290e5d2499fb16c5554b7b3adc
MamykinVsevolod <vsevolodmamykin@gmail.com>
2024-05-18 13:11:56 +0300

    fixed the transition to the mail app

--- app/src/main/java/com/f4/mypet/ui/screens/bugReport/BugReportScreen.kt
+++ app/src/main/java/com/f4/mypet/ui/screens/bugReport/BugReportScreen.kt
@@ -1,7 +1,7 @@
 package com.f4.mypet.ui.screens.bugReport
 
 import android.annotation.SuppressLint
-import android.content.Context
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
@@ -15,7 +15,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -33,14 +33,15 @@ import com.f4.mypet.ui.screens.bugReport.screenComponents.BugReportDescriptionFi
 import com.f4.mypet.ui.screens.bugReport.screenComponents.BugReportErrorField
 import com.f4.mypet.ui.screens.bugReport.screenComponents.CustomAlertDialog
 import com.f4.mypet.ui.screens.bugReport.screenComponents.SendButton
+import java.lang.NullPointerException
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter", "IntentReset",
     "UnusedMaterial3ScaffoldPaddingParameter"
 )
 @Composable
-fun BugReportScreen(navController: NavHostController, context: Context) {
-    //val context = LocalContext.current
+fun BugReportScreen(navController: NavHostController) {
+    val context = LocalContext.current
     var mutableSubject by remember { mutableStateOf("") }
     var mutableMessage by remember { mutableStateOf("") }
     var openAlertDialog by remember { mutableStateOf(false) }
@@ -54,12 +55,12 @@ fun BugReportScreen(navController: NavHostController, context: Context) {
                 onConfirmation = {
                     openAlertDialog = false
 
-                    navController.navigate(Routes.ListProfile.route) {
-                        popUpTo(Routes.ListProfile.route) {
-                            inclusive = true
-                        }
-                        launchSingleTop = true
-                    }
+//                    navController.navigate(Routes.ListProfile.route) {
+//                        popUpTo(Routes.ListProfile.route) {
+//                            inclusive = true
+//                        }
+//                        launchSingleTop = true
+//                    }
 
                     // логика создания наполнения письма и его отправки
 
@@ -70,7 +71,7 @@ fun BugReportScreen(navController: NavHostController, context: Context) {
 
                     try{
                         context.startActivity(Intent.createChooser(intent, "Send Email"))
-                    } catch (e: Exception) {
+                    } catch (e: ActivityNotFoundException) {
                         Toast.makeText(context, context.getString(R.string.bug_report_toast_error), Toast.LENGTH_SHORT).show()
                     }
                 },
@@ -84,9 +85,10 @@ fun BugReportScreen(navController: NavHostController, context: Context) {
         topBar = {
             MyPetTopBar(
                 text = stringResource(R.string.bug_report_title),
-                canNavigateBack = navController.previousBackStackEntry != null,
+                canNavigateBack = true,
                 navigateUp = { navController.navigateUp() },
                 actions = {}
+                //TODO возврат к главному экрану (список питомцев)
             )
         },
     ) { innerPadding ->
@@ -118,5 +120,3 @@ fun BugReportScreen(navController: NavHostController, context: Context) {
         }
     }
 }
-
-
